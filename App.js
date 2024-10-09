073bd0b3be75a3df6fda93b7e3de61b76a9f75e3
dinhchung2102 <vodinhchung4@gmail.com>
2024-10-09 11:38:13 +0700

    First commit

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { StyleSheet, Text, View } from 'react-native';
+import Screen1 from './screens/Screen1';
+import Screen2 from './screens/Screen2';
+import Screen3 from './screens/Screen3';
 
 export default function App() {
+  const Stack = createStackNavigator()
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer style={styles.container}>
+      <Stack.Navigator initialRouteName='Screen1' screenOptions={{headerShown: false}}>
+        <Stack.Screen name='Screen1'component={Screen1}/>
+        <Stack.Screen name='Screen2' component={Screen2}/>
+        <Stack.Screen name='Screen3' component={Screen3}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
